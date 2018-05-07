0536cbbf2c65adf815059aa4de9cbc48c1ee4fef
Michael Johnson <michael@ociaw.com>
2018-05-06 22:21:50 -0500

    Add more Composition tests

---
+++ AutoComposer.Tests/NullArgument.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoComposer.Tests
+{
+    [TestClass]
+    public class NullArgument
+    {
+        [TestMethod]
+        public void Enumerator()
+        {
+            Composer composer = new Composer();
+            Assert.ThrowsException<ArgumentNullException>(delegate { composer.Compose(null); });
+        }
+
+        [TestMethod]
+        public void GenericEnumerator()
+        {
+            Composer composer = new Composer();
+            IEnumerator<Object> enumerator = null;
+            Assert.ThrowsException<ArgumentNullException>(delegate { composer.Compose<Object>(enumerator); });
+        }
+
+        [TestMethod]
+        public void GenericArray()
+        {
+            Composer composer = new Composer();
+            Object[] array = null;
+            Assert.ThrowsException<ArgumentNullException>(delegate { composer.Compose<Object>(array); });
+        }
+    }
+}
--- AutoComposer.Tests/SingleComposition.cs
+++ AutoComposer.Tests/SingleComposition.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AutoComposer.Tests
@@ -37,11 +38,40 @@ namespace AutoComposer.Tests
         }
 
         [TestMethod]
+        public void DoubleNestWithNull()
+        {
+            Composer composer = new Composer();
+            Object[] objects = { null, new Middle(), new Bottom() };
+            Top top = composer.Compose<Top>(objects);
+            Assert.AreEqual(objects[0], top);
+        }
+
+        [TestMethod]
         public void BadDoubleNest()
         {
             Composer composer = new Composer();
             Object[] objects = { new Top(), new Middle() };
             Assert.ThrowsException<ArgumentException>(delegate { composer.Compose<Top>(objects); });
+        }
+
+        [TestMethod]
+        public void NoNestEnumerator()
+        {
+            Composer composer = new Composer();
+            Object[] objects = { new Bottom() };
+            Object bottom = composer.Compose(((IEnumerable<Object>)objects).GetEnumerator());
+            Assert.AreEqual(objects[0], bottom);
+        }
+
+        [TestMethod]
+        public void NoNestEnumeratorGeneric()
+        {
+            Composer composer = new Composer();
+            Object[] objects = { new Top(), new Middle(), new Bottom() };
+            Top top = composer.Compose<Top>(((IEnumerable<Object>)objects).GetEnumerator());
+            Assert.AreEqual(objects[0], top);
+            Assert.AreEqual(objects[1], top.Middle);
+            Assert.AreEqual(objects[2], top.Middle.Bottom);
         }
 
         private class Top
